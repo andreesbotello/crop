846a305037eb3d8880c911038d70052d6d30fef9
andreesbotello <botelloandrees@gmail.com>
2026-04-01 17:01:29 +0200

    para VPS

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -189,6 +189,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = 'static_root/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
